e2a263bcf1273afbba2fe66ee78725b67c9a763f
Dmitriy B <kupperfeet@gmail.com>
2022-08-09 16:39:43 +0300

    Example003_sum modifid

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,5 +1,10 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.Write("1 numbe = ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.Write("2 number = ");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.Write ("sum = ");
 Console.WriteLine(result);
 
